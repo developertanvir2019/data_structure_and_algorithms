1171e3f613e4f8da9711a3ea423308aac677a5aa
developertanvir2019 <developertanvir2019@gmail.com>
2023-11-22 00:32:36 +0600

    primary nuimber finder explore

---
+++ algorithms/Prime.js
@@ -0,0 +1,11 @@
+const prime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  for (i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
--- algorithms/factorial.js
+++ algorithms/factorial.js
@@ -1,6 +1,8 @@
 const factorial = (n) => {
   let result = 1;
   for (let i = 2; i <= n; i++) {
-    result = result + i;
+    result = result * i;
   }
+  return result;
 };
+console.log(factorial(0));
