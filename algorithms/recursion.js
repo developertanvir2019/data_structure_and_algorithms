d657491cdb236183e04357f41df2f1a98812647c
developertanvir2019 <developertanvir2019@gmail.com>
2023-11-22 23:13:59 +0600

    recursive fibonacci

---
+++ algorithms/recursion.js
@@ -0,0 +1,8 @@
+const recursiveFiboNacci = (n) => {
+  const fib = [0, 1];
+  for (i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+};
+console.log(recursiveFiboNacci(6));
