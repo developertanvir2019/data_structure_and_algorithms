dfc45ce936689852f7161399648187415cd010cf
developertanvir2019 <developertanvir2019@gmail.com>
2023-12-17 00:25:16 +0600

    complete stack

---
+++ data-structure/map.js
@@ -0,0 +1,9 @@
+// objects vs map
+// 1. Objects are unordered whereas maps are ordered
+// 2. keys in objects can only be string or symbol type whereas in maps, they can be of any type.
+
+const map = new Map([
+  ["a", 1],
+  ["b", 2],
+]);
+console.log(map);
---
+++ data-structure/stack.js
@@ -0,0 +1,7 @@
+// stack data structure always follow LIFO formed
+
+//  stack uses
+// 1browser history tracking
+// undo operation when typing
+// expression conversions
+// call stack in javaScript runtime
